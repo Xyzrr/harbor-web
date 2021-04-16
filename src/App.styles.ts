2f4e96942202973540de7a321c2469fb345e9856
John Qian <johnlongqian@gmail.com>
2021-04-15 23:42:10 -0700

    firebaseui with a bit of styling

---
+++ src/App.styles.ts
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+
+export const Logo = styled.h1`
+  color: #666;
+  margin-bottom: 8px;
+  font-size: 18px;
+  font-weight: 500;
+`;
+
+export const Wrapper = styled.div`
+  padding: 32px;
+  display: flex;
+  flex-direction: column;
+
+  max-width: 650px;
+  margin: 0 auto;
+  margin-top: 48px;
+
+  .firebaseui-page-provider-sign-in {
+    margin: 0;
+    .firebaseui-card-content {
+      padding: 0;
+    }
+    .firebaseui-list-item {
+      text-align: left;
+      &:last-child {
+        &::before {
+          content: "or ";
+          font-size: 14px;
+          color: #666;
+        }
+        &::after {
+          content: ".";
+          font-size: 14px;
+          color: #666;
+        }
+      }
+    }
+    .firebaseui-idp-button {
+      max-width: none;
+    }
+    .firebaseui-idp-google {
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: none;
+      border: 2px solid #ccc;
+      &:hover {
+        border: 2px solid #666;
+      }
+      .firebaseui-idp-text {
+        font-size: 18px;
+        color: #444;
+      }
+    }
+    .firebaseui-idp-password {
+      background: none !important;
+      padding: 0;
+      box-shadow: none;
+      display: inline;
+      min-height: auto;
+      width: fit-content;
+      .firebaseui-idp-icon-wrapper {
+        display: none;
+      }
+      .firebaseui-idp-text {
+        color: #666;
+        padding: 0;
+        text-decoration: underline;
+        font-weight: 400;
+        text-transform: lowercase;
+        transform: translateY(-1px);
+        &:hover {
+          color: #222;
+        }
+      }
+    }
+    .firebaseui-card-footer {
+      padding: 0;
+    }
+    .firebaseui-tos {
+      border-top: 1px solid #eee;
+      padding-top: 24px;
+      margin-top: 28px;
+      text-align: left;
+      font-size: 14px;
+      line-height: 20px;
+    }
+  }
+
+  .firebaseui-id-page-password-sign-in,
+  .firebaseui-id-page-sign-in,
+  .firebaseui-id-page-password-sign-up,
+  .firebaseui-id-page-password-recovery,
+  .firebaseui-id-page-password-recovery-email-sent,
+  .firebaseui-id-page-callback {
+    margin: 0;
+    box-shadow: none;
+    border: 2px solid #ccc;
+    border-radius: 8px;
+    margin-top: 16px;
+
+    button {
+      box-shadow: none;
+      border-radius: 8px;
+    }
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as S from "./App.styles";
+
+import React from "react";
+import logo from "./logo.svg";
+import firebase from "firebase";
+import * as firebaseui from "firebaseui";
+import "firebaseui/dist/firebaseui.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA89oz2--yQCG8AieZNa_7j-gPcJsBFyEA",
+  authDomain: "harbor-chat.firebaseapp.com",
+  projectId: "harbor-chat",
+  storageBucket: "harbor-chat.appspot.com",
+  messagingSenderId: "222349427903",
+  appId: "1:222349427903:web:6433be4de99f7c5df5720a",
+  measurementId: "G-5C5J3GGJ06",
+};
+
+var uiConfig = {
+  signInSuccessUrl: "<url-to-redirect-to-on-success>",
+  signInOptions: [
+    // Leave the lines as is for the providers you want to offer your users.
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+  ],
+  // tosUrl and privacyPolicyUrl accept either url string or a callback
+  // function.
+  // Terms of service url/callback.
+  tosUrl: "<your-tos-url>",
+  // Privacy policy url/callback.
+  privacyPolicyUrl: function () {
+    window.location.assign("<your-privacy-policy-url>");
+  },
+};
 
 function App() {
+  const firebaseUiWrapperRef = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    const firebaseUiWrapperEl = firebaseUiWrapperRef.current;
+    if (firebaseUiWrapperEl == null) {
+      return;
+    }
+
+    const app = firebase.initializeApp(firebaseConfig, "Harbor Web");
+
+    const ui = new firebaseui.auth.AuthUI(app.auth());
+    ui.start(firebaseUiWrapperEl, uiConfig);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <S.Wrapper>
+      <S.Logo>Sign in to Harbor to continue</S.Logo>
+      <div ref={firebaseUiWrapperRef}></div>
+    </S.Wrapper>
   );
 }
 
